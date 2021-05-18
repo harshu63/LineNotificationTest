4548e9999597a128eebcc51fd846904ca13a4884
harshu63 <43599965+harshu63@users.noreply.github.com>
2021-05-18 11:23:06 +0530

    Update LineLoginConfig.java

--- src/main/java/com/notification/config/LineLoginConfig.java
+++ src/main/java/com/notification/config/LineLoginConfig.java
@@ -10,6 +10,7 @@ import org.springframework.security.oauth2.client.registration.ClientRegistratio
 import org.springframework.security.oauth2.client.registration.InMemoryClientRegistrationRepository;
 import org.springframework.security.oauth2.core.AuthorizationGrantType;
 import org.springframework.security.oauth2.core.ClientAuthenticationMethod;
+import org.springframework.beans.factory.annotation.Value;
 
 @Configuration
 public class LineLoginConfig {
